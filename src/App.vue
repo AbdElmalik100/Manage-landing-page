8dd73949faa751e39106c60a11ec9636d5f66e2d
Abd Elmalik Abd Elghafar <paydaychains100@gmail.com>
2023-05-02 04:47:19 +0300

    adding challenge 10 files

---
+++ src/App.vue
@@ -0,0 +1,519 @@
+<template >
+  <main>
+    <HeaderNav></HeaderNav>
+    <div class="landing py-5">
+      <div class="container d-flex align-items-center gap-5 flex-column-reverse flex-md-row">
+        <div class="info text-md-start text-center">
+          <h1 class="mb-0 fw-bold">Bring every together to build better products.</h1>
+          <p class="my-5 w-75">
+            Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goal in view
+          </p>
+          <button class="butn">Get Started</button>
+        </div>
+        <div class="image">
+          <img src="/images/illustration-intro.svg" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="about py-5">
+      <div class="container d-flex align-items-start gap-5 flex-column flex-md-row">
+        <div class="left">
+          <h1 class="mb-5 text-md-start text-center">What' different about Manage?</h1>
+          <p class="text-md-start text-center">
+            Manage provides all the functionality your team needs, without the complexity. Our software is tailor-made
+            for modern digital product teams.
+          </p>
+        </div>
+        <div class="right desk">
+          <div class="box d-flex align-items-start gap-4 ms-auto w-75">
+            <span class="p-2 px-4 text-light rounded-pill fw-bold">01</span>
+            <div class="text">
+              <h4 class="mt-2 mb-3 fs-5">Track company-wide progress</h4>
+              <p>
+                See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level
+                all the way done to the smallest of details. Never lose sight of the bigger picture again.
+              </p>
+            </div>
+          </div>
+          <div class="box d-flex align-items-start gap-4 ms-auto w-75">
+            <span class="p-2 px-4 text-light rounded-pill fw-bold">02</span>
+            <div class="text">
+              <h4 class="mt-2 mb-3 fs-5">Advanced built-in reports</h4>
+              <p>
+                Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps
+                you build out the reports you need to keep key stakeholders informed.
+              </p>
+            </div>
+          </div>
+          <div class="box d-flex align-items-start gap-4 ms-auto w-75">
+            <span class="p-2 px-4 text-light rounded-pill fw-bold">03</span>
+            <div class="text">
+              <h4 class="mt-2 mb-3 fs-5">Everything you need in one place</h4>
+              <p>
+                Stop jumping from one service to another to communicate, store files, track tasks and share documents.
+                Manage offers an all-in-one team productivity solution.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="right mobile">
+          <div class="box d-flex flex-column">
+            <div class="d-flex gap-3">
+              <span class="p-2 px-4 text-light rounded-pill fw-bold">01</span>
+              <h4 class="mb-0 fs-5 flex-fill d-flex align-items-center justify-content-start rounded-pill">Track
+                company-wide progress</h4>
+            </div>
+            <div class="text mt-3">
+              <p>
+                See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level
+                all the way done to the smallest of details. Never lose sight of the bigger picture again.
+              </p>
+            </div>
+          </div>
+          <div class="box d-flex flex-column">
+            <div class="d-flex gap-3">
+              <span class="p-2 px-4 text-light rounded-pill fw-bold">02</span>
+              <h4 class="mb-0 fs-5 flex-fill d-flex align-items-center justify-content-start rounded-pill">Advanced
+                built-in reports</h4>
+            </div>
+            <div class="text mt-3">
+              <p>
+                Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps
+                you build out the reports you need to keep key stakeholders informed.
+              </p>
+            </div>
+          </div>
+          <div class="box d-flex flex-column">
+            <div class="d-flex gap-3">
+              <span class="p-2 px-4 text-light rounded-pill fw-bold">03</span>
+              <h4 class="mb-0 fs-5 flex-fill d-flex align-items-center justify-content-start rounded-pill">Everything you
+                need in one place</h4>
+            </div>
+            <div class="text mt-3">
+              <p>
+                Stop jumping from one service to another to communicate, store files, track tasks and share documents.
+                Manage offers an all-in-one team productivity solution.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="testimonials py-5 position-relative">
+      <img class="bg" src="/images/bg-tablet-pattern.svg" alt="">
+      <div class="wrapper overflow-hidden py-5">
+        <h1 class="mb-5 text-center">What they've said</h1>
+        <div class="boxes-cont desk py-5">
+          <div class="slider d-flex align-items-ceter gap-3">
+            <div class="box one position-relative col-md-4 p-4 rounded-1 text-center">
+              <img class="position-absolute" width="75" src="/images/avatar-anisha.png" alt="">
+              <h4 class="fs-5 my-4">Anisha Li</h4>
+              <p>
+                “Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at
+                all
+                times keeps everyone motivated.”
+              </p>
+            </div>
+            <div class="box two position-relative col-md-4 p-4 rounded-1 text-center">
+              <img class="position-absolute" width="75" src="/images/avatar-ali.png" alt="">
+              <h4 class="fs-5 my-4">Ali Bravo</h4>
+              <p>
+                “We have been able to cancel so many other subscriptions since using Manage. There is no more
+                cross-channel
+                confusion and everyone is much more focused.”
+              </p>
+            </div>
+            <div class="box three position-relative col-md-4 p-4 rounded-1 text-center">
+              <img class="position-absolute" width="75" src="/images/avatar-richard.png" alt="">
+              <h4 class="fs-5 my-4">Richard Watts</h4>
+              <p>
+                “Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop
+                recommending them to everyone I talk to!”
+              </p>
+            </div>
+            <div class="box foure position-relative col-md-4 p-4 rounded-1 text-center">
+              <img class="position-absolute" width="75" src="/images/avatar-shanai.png" alt="">
+              <h4 class="fs-5 my-4">Shanai Gough</h4>
+              <p>
+                “Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the
+                whole
+                team in-sync without being intrusive.”
+              </p>
+            </div>
+            <div class="box one position-relative col-md-4 p-4 rounded-1 text-center">
+              <img class="position-absolute" width="75" src="/images/avatar-anisha.png" alt="">
+              <h4 class="fs-5 my-4">Anisha Li</h4>
+              <p>
+                “Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at
+                all
+                times keeps everyone motivated.”
+              </p>
+            </div>
+            <div class="box two position-relative col-md-4 p-4 rounded-1 text-center">
+              <img class="position-absolute" width="75" src="/images/avatar-ali.png" alt="">
+              <h4 class="fs-5 my-4">Ali Bravo</h4>
+              <p>
+                “We have been able to cancel so many other subscriptions since using Manage. There is no more
+                cross-channel
+                confusion and everyone is much more focused.”
+              </p>
+            </div>
+            <div class="box three position-relative col-md-4 p-4 rounded-1 text-center">
+              <img class="position-absolute" width="75" src="/images/avatar-richard.png" alt="">
+              <h4 class="fs-5 my-4">Richard Watts</h4>
+              <p>
+                “Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop
+                recommending them to everyone I talk to!”
+              </p>
+            </div>
+            <div class="box foure position-relative col-md-4 p-4 rounded-1 text-center">
+              <img class="position-absolute" width="75" src="/images/avatar-shanai.png" alt="">
+              <h4 class="fs-5 my-4">Shanai Gough</h4>
+              <p>
+                “Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the
+                whole
+                team in-sync without being intrusive.”
+              </p>
+            </div>
+          </div>
+        </div>
+        <div id="carouselExampleIndicators" class="carousel slide boxes-cont mobile py-5" data-bs-ride="true">
+          <div class="carousel-indicators align-items-center">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+              aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+              aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+              aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+              aria-label="Slide 4"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="box one mb-5 position-relative col-md-4 p-4 rounded-1 text-center">
+                <img class="position-absolute" width="75" src="/images/avatar-anisha.png" alt="">
+                <h4 class="fs-5 my-4">Anisha Li</h4>
+                <p>
+                  “Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at
+                  all
+                  times keeps everyone motivated.”
+                </p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="box two mb-5 position-relative col-md-4 p-4 rounded-1 text-center">
+                <img class="position-absolute" width="75" src="/images/avatar-ali.png" alt="">
+                <h4 class="fs-5 my-4">Ali Bravo</h4>
+                <p>
+                  “We have been able to cancel so many other subscriptions since using Manage. There is no more
+                  cross-channel
+                  confusion and everyone is much more focused.”
+                </p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="box three mb-5 position-relative col-md-4 p-4 rounded-1 text-center">
+                <img class="position-absolute" width="75" src="/images/avatar-richard.png" alt="">
+                <h4 class="fs-5 my-4">Richard Watts</h4>
+                <p>
+                  “Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop
+                  recommending them to everyone I talk to!”
+                </p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="box foure mb-5 position-relative col-md-4 p-4 rounded-1 text-center">
+                <img class="position-absolute" width="75" src="/images/avatar-shanai.png" alt="">
+                <h4 class="fs-5 my-4">Shanai Gough</h4>
+                <p>
+                  “Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the
+                  whole
+                  team in-sync without being intrusive.”
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button class="butn text-center d-block m-auto mt-5">Get Started</button>
+      </div>
+    </div>
+    <div class="checkout py-5">
+      <div
+        class="container d-flex align-items-center justify-content-between flex-md-row flex-column text-md-start text-center gap-4">
+        <h1 class="mb- fs-1 w-50">Simplify how your team works today.</h1>
+        <button class="butn">Get Started</button>
+      </div>
+    </div>
+    <FooterComp></FooterComp>
+  </main>
+</template>
+<script setup>
+import HeaderNav from './components/HeaderNav.vue';
+import FooterComp from './components/FooterComp.vue'
+
+</script>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+:root {
+  // ### Primary
+  --Bright-Red: hsl(12, 88%, 59%);
+  --Dark-Blue: hsl(228, 39%, 23%);
+
+  // ### Neutral
+  --Dark-Grayish-Blue: hsl(227, 12%, 61%);
+  --Very-Dark-Blue: hsl(233, 12%, 13%);
+  --Very-Pale-Red: hsl(13, 100%, 96%);
+  --Vary-Light-Gray: hsl(0, 0%, 98%);
+}
+
+body {
+  font-size: 16px;
+  font-family: 'Be Vietnam Pro', sans-serif;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.butn {
+  border: none;
+  outline: none;
+  background-color: var(--Bright-Red);
+  border-radius: 50px;
+  color: var(--Vary-Light-Gray);
+  padding: 12px 35px;
+  transition: 0.2s;
+  box-shadow: 0 5px 15px var(--Bright-Red);
+
+  &:hover {
+    opacity: 0.7;
+  }
+}
+
+
+main {
+  background-image: url(/images/bg-tablet-pattern.svg);
+  min-height: 100vh;
+  background-repeat: no-repeat;
+  background-position: 125% -10%;
+
+  .landing {
+    .info {
+      h1 {
+        color: var(--Dark-Blue);
+        font-size: 55px;
+      }
+
+      p {
+        color: var(--Dark-Grayish-Blue);
+      }
+    }
+  }
+
+  .about {
+    .left {
+      h1 {
+        color: var(--Dark-Blue);
+      }
+
+      p {
+        color: var(--Dark-Grayish-Blue);
+        line-height: 1.7;
+      }
+    }
+
+    .right.mobile {
+      display: none;
+    }
+
+    .right {
+      .box {
+        &:not(:last-of-type) {
+          margin-bottom: 25px;
+        }
+
+        span {
+          background-color: var(--Bright-Red);
+        }
+
+        .text {
+          h4 {
+            color: var(--Dark-Blue);
+          }
+
+          p {
+            color: var(--Dark-Grayish-Blue);
+            line-height: 1.7;
+          }
+        }
+      }
+    }
+  }
+
+  .testimonials {
+    .bg {
+      position: absolute;
+      top: -65%;
+      left: -30%;
+      z-index: -150;
+    }
+
+    h1 {
+      color: var(--Dark-Blue);
+    }
+
+    .boxes-cont {
+      padding-top: 50px !important;
+      overflow: hidden;
+
+      &.mobile {
+        display: none;
+      }
+
+      .slider {
+        animation: scroll 25s linear infinite;
+
+        .box {
+          background-color: var(--Vary-Light-Gray);
+
+          img {
+            top: -37px;
+            left: 50%;
+            transform: translateX(-50%);
+          }
+
+          h4 {
+            color: var(--Dark-Blue);
+          }
+
+          p {
+            color: var(--Dark-Grayish-Blue);
+          }
+        }
+      }
+    }
+  }
+
+  .checkout {
+    background-color: var(--Bright-Red);
+    background-image: url(/images/bg-simplify-section-desktop.svg);
+
+    h1 {
+      color: var(--Vary-Light-Gray);
+    }
+
+    button {
+      background-color: var(--Vary-Light-Gray);
+      color: var(--Bright-Red);
+
+      &:hover {
+        opacity: 1;
+        filter: brightness(1.3);
+      }
+    }
+  }
+
+  @media (max-width: 991px) {
+    background-size: 100%;
+    overflow: hidden;
+    background-position-x: 60px;
+    background-position-y: -1%;
+
+    .landing {
+      .info {
+        p {
+          width: 100% !important;
+        }
+      }
+    }
+
+    .about {
+      .right.desk {
+        display: none;
+      }
+
+      .right.mobile {
+        display: block;
+
+        .box:not(:last-of-type) {
+          margin-bottom: 50px;
+        }
+
+        h4 {
+          background-color: var(--Very-Pale-Red);
+          position: relative;
+          left: -75px;
+          z-index: -1;
+          padding-left: 70px;
+          color: var(--Dark-Blue);
+          font-size: 15px !important;
+          width: 330px;
+        }
+      }
+    }
+
+    .testimonials {
+      .bg {
+        top: -200%;
+        left: 30%;
+        width: 100%;
+      }
+
+      .boxes-cont.desk {
+        display: none;
+      }
+
+      .boxes-cont.mobile {
+        display: block;
+        .carousel-indicators {
+          button {
+            border: 2px solid var(--Bright-Red);
+            width: 10px;
+            height: 10px;
+            border-radius: 50%;
+            &.active {
+              background-color: var(--Bright-Red);
+            }
+          }
+        }
+        .carousel-inner {
+          overflow: visible;
+          .box {
+            background-color: var(--Vary-Light-Gray);
+            width: calc(100% - 25px);
+            margin: auto;
+            img {
+              top: -37px;
+              left: 50%;
+              transform: translateX(-50%);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @media (max-width: 767px) {
+    .landing {
+      img {
+        width: 100%;
+      }
+    }
+  }
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateX(0);
+  }
+
+  100% {
+    transform: translateX(calc(-465.66px * 4));
+  }
+}
+</style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,114 @@
+<template>
+    <header>
+        <div :class="`overlay ${open ? 'active' : ''}`"></div>
+        <div class="container d-flex align-items-center justify-content-between py-5">
+            <img src="/images/logo.svg" alt="">
+            <ul :class="`d-flex align-items-center gap-4 ${open ? 'active' : ''}`">
+                <li>Pricing</li>
+                <li>Product</li>
+                <li>About Us</li>
+                <li>Careers</li>
+                <li>Community</li>
+            </ul>
+            <button class="butn">Get Started</button>
+            <img @click="open = true" :class="`menu ${open ? 'active' : ''}`" src="/images/icon-hamburger.svg"
+                alt="">
+            <img @click="open = false" :class="`close ${open ? 'active' : ''}`" src="/images/icon-close.svg"
+                alt="">
+        </div>
+    </header>
+</template>
+<script setup>
+import { ref, watch } from 'vue';
+
+const open = ref(false)
+
+watch(open, newVal => {
+    if (newVal === true) {
+        document.body.style.overflow = 'hidden'
+    } else document.body.style.overflow = 'visible'
+})
+
+
+</script>
+<style lang="scss" scoped>
+.overlay {
+    display: none;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background: linear-gradient(to bottom, rgba(116, 116, 116, 0.253), rgba(0, 0, 0, 0.699));
+}
+
+.close,
+.menu {
+    display: none;
+}
+
+header {
+
+    ul {
+        li {
+            cursor: pointer;
+            transition: 0.2s;
+            color: var(--Very-Dark-Blue);
+
+            &:hover {
+                color: var(--Dark-Grayish-Blue);
+            }
+        }
+    }
+
+    @media (max-width:991px) {
+
+        .close,
+        .menu {
+            cursor: pointer;
+            position: relative;
+            z-index: 55555;
+        }
+
+        .menu {
+            display: block;
+
+            &.active {
+                display: none;
+            }
+        }
+
+        .overlay.active {
+            display: block;
+        }
+
+        .close.active {
+            display: block;
+        }
+
+        ul {
+            position: absolute;
+            background-color: white;
+            flex-direction: column;
+            width: calc(100% - 24px);
+            padding: 50px;
+            left: 50%;
+            transform: translateX(-50%);
+            top: -100%;
+            border-radius: 5px;
+            transition: 0.2s;
+            li {
+                font-weight: bold;
+            }
+
+            &.active {
+                top: 20%;
+
+            }
+        }
+
+        button {
+            display: none;
+        }
+    }
+}</style>
